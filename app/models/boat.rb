02df22e2ad0f51f2ce9a569149d1eaf8c53751cb
Diana Badas <diana.badas@gmail.com>
2019-06-07 13:51:39 +0000

    Automatically backed up by Learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -26,4 +26,8 @@ class Boat < ActiveRecord::Base
   def self.sailboats
     includes(:classifications).where(classifications: { name: 'Sailboat' })
   end
+
+  def self.with_three_classifications
+
+  end
 end
