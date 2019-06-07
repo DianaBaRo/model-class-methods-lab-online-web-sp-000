6a013b2382b819d7a901a2467d388b887cc2a5d1
Diana Badas <diana.badas@gmail.com>
2019-06-07 14:28:11 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,7 +1,11 @@
 class Captain < ActiveRecord::Base
   has_many :boats
 
-  def catamaran_operators
+  def self.catamaran_operators
     joins(boats: :classifications).where(classifications: { name: 'Catamaran' })
+  end
+
+  def self.sailors
+
   end
 end
